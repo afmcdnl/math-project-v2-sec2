4b87e3de5c13cfe88e13b859f537c997a18eb002
Andrew <afmcdnl@gmail.com>
2021-10-26 21:28:31 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    } else if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+radio.setGroup(2)
